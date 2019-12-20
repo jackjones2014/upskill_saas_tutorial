91122138b924309c68f539153a1426865616b856
Jack Jones <jackjones2014@gmail.com>
2019-12-19 20:05:15 -0500

    Fixed environment file syntax

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,9 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
 	:port				=> ENV['MAILGUN_SMTP_PORT'],
 	:address			=> ENV['MAILGUN_SMTP_SERVER'],
-	:user_name			=> ENV['MAILGUN_SMTP_LOGIN']
+	:user_name			=> ENV['MAILGUN_SMTP_LOGIN'],
 	:password			=> ENV['MAILGUN_SMTP_PASSWORD']
-	:domain				=> 'https://vast-chamber-68837.herokuapp.com/'
+	:domain				=> 'https://vast-chamber-68837.herokuapp.com/',
 	:authentication		=> :plain,
 }
 ActionMailer::Base.delivery_method = :smtp 
